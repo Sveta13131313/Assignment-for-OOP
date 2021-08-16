7aa652652017f8169c2ec2c7ac4a7ac7dd8fa54a
Svetlana Khaydoshkina <sveta13131313@yandex.ru>
2021-08-16 17:02:10 +0300

    Задание под вопросом

--- script.js
+++ script.js
@@ -1,47 +1,44 @@
 'use strict'
 
-const DomElement = function (selector, height, width, bg, fontSize) {
-    this.selector = selector;
+const DomElement = function (height, width, bg, position) {
     this.height = height;
     this.width = width;
     this.bg = bg;
-    this.fontSize = fontSize;
+    this.position = position;
 
-    if (this.selector[0] == '.') {
 
-        DomElement.prototype.createElementDiv = function (text) {
-            this.text = text;
-            this.selector=this.selector.slice(1);
-            this.notification = document.createElement('div');
-            this.notification.classList.add(this.selector);
-            this.notification.style.width = this.width + 'px';
-            this.notification.style.height = this.height + 'px';
-            this.notification.style.backgroundColor = this.bg;
-            this.notification.style.fontSize = this.fontSize + 'px';
-            this.notification.innerHTML = this.text;
-            document.body.append(this.notification);
-        }
+    DomElement.prototype.createElementDiv = function () {
+        this.notification = document.createElement('div');
+        this.notification.classList.add(this.selector);
+  
+        this.notification.style.width = this.width + 'px';
+        this.notification.style.height = this.height + 'px';
+        this.notification.style.backgroundColor = this.bg;
+        this.notification.style.fontSize = this.fontSize + 'px';
+        document.body.append(this.notification);
     }
-    else if (this.selector[0] == '#') {
 
-        DomElement.prototype.createElementDiv = function (text) {
-            this.text = text;
-            this.selector=this.selector.slice(1);
-            this.notification = document.createElement('div');
-            this.notification.setAttribute('id',this.selector);
-            this.notification.style.width = this.width + 'px';
-            this.notification.style.height = this.height + 'px';
-            this.notification.style.backgroundColor = this.bg;
-            this.notification.style.fontSize = this.fontSize + 'px';
-            this.notification.innerHTML = this.text;
-            document.body.append(this.notification);
-        }
-
-    }
 }
 
-const newDiv = new DomElement('.block', '400', '400', 'blue', '30');
-newDiv.createElementDiv('Привет мир!');
+const newDiv = new DomElement('100', '100', 'blue', ' absolute');
+document.addEventListener("DOMContentLoaded", function() {
+    newDiv.createElementDiv();
+  });
 
-const newId = new DomElement('#best', '400', '400', 'green', '30');
-newId.createElementDiv('Привет мир!');
+
+
+document.addEventListener('keydown', function (event) {
+        if (event.code == 'ArrowUp' ) {
+            newDiv.notification.style.top += 10 + 'px';
+        }
+        if (event.code == 'ArrowLeft') {
+            newDiv.notification.style.left += 10 + 'px';
+        }
+        if (event.code == 'ArrowRight') {
+            newDiv.notification.style.left -= 10 + 'px';
+        }
+        if (event.code == 'ArrowDown') {
+            newDiv.notification.style.top -= 10 + 'px';
+        }
+
+    });
