f29cf9531ee978cbaf3e666a7d1914dd9f5898fa
Svetlana Khaydoshkina <sveta13131313@yandex.ru>
2021-08-17 00:09:51 +0300

    Задание исправлено

--- script.js
+++ script.js
@@ -7,39 +7,25 @@ const DomElement = function (selector, height, width, bg, fontSize) {
     this.bg = bg;
     this.fontSize = fontSize;
 
-    if (this.selector[0] == '.') {
+    DomElement.prototype.createElementDiv = function (text) {
+        this.text = text;
+        this.selector = this.selector.slice(1);
+        this.notification = document.createElement('div');
+        this.notification.style.width = this.width + 'px';
+        this.notification.style.height = this.height + 'px';
+        this.notification.style.backgroundColor = this.bg;
+        this.notification.style.fontSize = this.fontSize + 'px';
+        this.notification.innerHTML = this.text;
 
-        DomElement.prototype.createElementDiv = function (text) {
-            this.text = text;
-            this.selector=this.selector.slice(1);
-            this.notification = document.createElement('div');
+        if (this.selector[0] == '.') {
             this.notification.classList.add(this.selector);
-            this.notification.style.width = this.width + 'px';
-            this.notification.style.height = this.height + 'px';
-            this.notification.style.backgroundColor = this.bg;
-            this.notification.style.fontSize = this.fontSize + 'px';
-            this.notification.innerHTML = this.text;
-            document.body.append(this.notification);
+        } else if (this.selector[0] == '#') {
+            this.notification.setAttribute('id', this.selector);
         }
-    }
-    else if (this.selector[0] == '#') {
-
-        DomElement.prototype.createElementDiv = function (text) {
-            this.text = text;
-            this.selector=this.selector.slice(1);
-            this.notification = document.createElement('div');
-            this.notification.setAttribute('id',this.selector);
-            this.notification.style.width = this.width + 'px';
-            this.notification.style.height = this.height + 'px';
-            this.notification.style.backgroundColor = this.bg;
-            this.notification.style.fontSize = this.fontSize + 'px';
-            this.notification.innerHTML = this.text;
-            document.body.append(this.notification);
-        }
-
+        document.body.append(this.notification);
     }
 }
-
+   
 const newDiv = new DomElement('.block', '400', '400', 'blue', '30');
 newDiv.createElementDiv('Привет мир!');
 
